1d66ade843748c7f382e98d66158905685470092
Bondarenko Anna <bondanyta@gmail.com>
2026-08-19 12:27:46 +0300

    Етап 8: кеш перекладів зі збереженням у файл

--- translator.py
+++ translator.py
@@ -1,8 +1,9 @@
+
 """
 Етап 7: переклад. Локальна модель Helsinki-NLP/opus-mt-en-uk.
 Поки просто перевіряємо, що переклад працює.
 """
-
+import json
 from transformers import MarianMTModel, MarianTokenizer
 
 MODEL_NAME = "Helsinki-NLP/opus-mt-en-uk"  # англійська → українська
@@ -12,15 +13,27 @@ MODEL_NAME = "Helsinki-NLP/opus-mt-en-uk"  # англійська → украї
 tokenizer = MarianTokenizer.from_pretrained(MODEL_NAME)
 model = MarianMTModel.from_pretrained(MODEL_NAME)
 
+try:
+    with open("cache.json","r", encoding="utf-8") as f:
+        cache=json.load(f)
+except FileNotFoundError: 
+    cache ={}
 
 def translate(text):
     """Перекладає англійський текст українською."""
-    tokens = tokenizer(text, return_tensors="pt")   # текст → числа (тензори)
-    output = model.generate(**tokens)                # модель перекладає
+    if text in cache:
+        result = cache[text]
+    else:
+        tokens = tokenizer(text, return_tensors="pt")   # текст → числа (тензори)
+        output = model.generate(**tokens)                # модель перекладає
     # числа → текст; skip_special_tokens прибирає службові токени
-    result = tokenizer.decode(output[0], skip_special_tokens=True)
+        result = tokenizer.decode(output[0], skip_special_tokens=True)
+        cache[text] = result
+        with open("cache.json", "w", encoding="utf-8")as f:
+            json.dump(cache, f, ensure_ascii=False) 
     return result
 
 
 if __name__ == "__main__":
+    print(translate("Back then we were young and unafraid."))
     print(translate("Back then we were young and unafraid."))
